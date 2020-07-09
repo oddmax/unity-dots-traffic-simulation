d251b2faa3fcf4d2dcd0286059bf909a9a853e20
Maxim Priakhin <maxim.priakhin@innogames.com>
2020-07-09 18:36:53 +0200

    add RandomSystem to get random numbers

--- Assets/TrafficSimulation/Systems/RandomSystem.cs
+++ Assets/TrafficSimulation/Systems/RandomSystem.cs
@@ -1,4 +1,31 @@
-$HEADER$namespace $NAMESPACE$
+using Unity.Collections;
+using Unity.Entities;
+using Unity.Jobs.LowLevel.Unsafe;
+
+namespace TrafficSimulation.Systems
 {
-  public class $CLASS$ {$END$}
+    /// <summary>
+    /// https://reeseschultz.com/random-number-generation-with-unity-dots/
+    /// </summary>
+    [UpdateInGroup(typeof(InitializationSystemGroup))]
+    class RandomSystem : ComponentSystem
+    {
+        public NativeArray<Unity.Mathematics.Random> RandomArray { get; private set; }
+
+        protected override void OnCreate()
+        {
+            var randomArray = new Unity.Mathematics.Random[JobsUtility.MaxJobThreadCount];
+            var seed = new System.Random();
+
+            for (int i = 0; i < JobsUtility.MaxJobThreadCount; ++i)
+                randomArray[i] = new Unity.Mathematics.Random((uint)seed.Next());
+
+            RandomArray = new NativeArray<Unity.Mathematics.Random>(randomArray, Allocator.Persistent);
+        }
+
+        protected override void OnDestroy()
+            => RandomArray.Dispose();
+
+        protected override void OnUpdate() { }
+    }
 }
