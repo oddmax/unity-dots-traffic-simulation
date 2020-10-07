ede3e5ebb9f7cf54656559137c34dd38ce4b0ece
Maxim Priakhin <maxim.priakhin@innogames.com>
2020-10-07 23:04:58 +0200

    add find car in front in segment helper

---
+++ Assets/TrafficSimulation/Systems/CarsInSegmentsFindHelper.cs
@@ -0,0 +1,42 @@
+using Unity.Collections;
+using Unity.Entities;
+
+namespace TrafficSimulation.Systems
+{
+    public struct CarsInSegmentsFindHelper
+    {
+        public void FindVehicleInFrontInSegment(
+            NativeMultiHashMap<Entity, VehicleSegmentData> vehicleSegmentMap,
+            Entity segmentEntity,
+            float vehicleHeadPosition,
+            ref Entity nextVehicleEntity,
+            ref float nextVehicleBackPosition
+        )
+        {
+            NativeMultiHashMapIterator<Entity> nativeMultiHashMapIterator;
+            if (vehicleSegmentMap.TryGetFirstValue(segmentEntity, out var segmentData, out nativeMultiHashMapIterator))
+            {
+                do
+                {
+                    if (!(vehicleHeadPosition < segmentData.BackSegPosition)) 
+                        continue;
+                    
+                    if (nextVehicleEntity == Entity.Null)
+                    {
+                        //no next vehicle, assign
+                        nextVehicleEntity = segmentData.Entity;
+                        nextVehicleBackPosition = segmentData.BackSegPosition;
+                    }
+                    else
+                    {
+                        if (segmentData.BackSegPosition < nextVehicleBackPosition)
+                        {
+                            nextVehicleEntity = segmentData.Entity;
+                            nextVehicleBackPosition = segmentData.BackSegPosition;
+                        }
+                    }
+                } while (vehicleSegmentMap.TryGetNextValue(out segmentData, ref nativeMultiHashMapIterator));
+            }
+        }
+    }
+}
