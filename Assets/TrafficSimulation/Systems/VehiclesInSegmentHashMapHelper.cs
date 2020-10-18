cd3a5a84e2f7e1b654edb1926c35e422d7e9b994
Maxim Priakhin <maxim.priakhin@innogames.com>
2020-10-18 21:21:24 +0200

    add spawning with proper positon and rotation

--- Assets/TrafficSimulation/Systems/VehiclesInSegmentHashMapHelper.cs
+++ Assets/TrafficSimulation/Systems/VehiclesInSegmentHashMapHelper.cs
@@ -41,6 +41,35 @@ namespace TrafficSimulation.Systems
                 } while (vehicleSegmentMap.TryGetNextValue(out segmentData, ref nativeMultiHashMapIterator));
             }
         }
+        
+        public bool IsSpaceAvailableAt(
+            NativeMultiHashMap<Entity, VehicleSegmentData> vehicleSegmentMap,
+            Entity segmentEntity,
+            float position,
+            float vehicleSize
+        )
+        {
+            NativeMultiHashMapIterator<Entity> nativeMultiHashMapIterator;
+            var vehicleFrontPos = position + vehicleSize / 2;
+            var vehicleBackPos = vehicleFrontPos - vehicleSize;
+            var canFit = true;
+            if (vehicleSegmentMap.TryGetFirstValue(segmentEntity, out var segmentData, out nativeMultiHashMapIterator))
+            {
+                do
+                {
+                    if (vehicleFrontPos < segmentData.BackSegPosition)
+                        continue;
+                    
+                    if(vehicleBackPos > segmentData.BackSegPosition + segmentData.VehicleSize)
+                        continue;
+
+                    canFit = false;
+
+                } while (vehicleSegmentMap.TryGetNextValue(out segmentData, ref nativeMultiHashMapIterator));
+            }
+
+            return canFit;
+        }
 
         public bool HasVehicleInSegment(
             NativeMultiHashMap<Entity, VehicleSegmentData> vehicleSegmentMap,
--- Assets/TrafficSimulation/TrafficSpawner.cs
+++ Assets/TrafficSimulation/TrafficSpawner.cs
@@ -1,10 +1,14 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using TrafficSimulation;
 using TrafficSimulation.Components.Buffers;
 using TrafficSimulation.Components.Road;
 using TrafficSimulation.Components.Vehicle;
 using TrafficSimulation.RoadNetworkSetup;
+using TrafficSimulation.Systems;
 using Unity.Entities;
+using Unity.Mathematics;
+using Unity.Transforms;
 using UnityEngine;
 using Random = UnityEngine.Random;
 
@@ -15,60 +19,102 @@ public class TrafficSpawner : MonoBehaviour, IConvertGameObjectToEntity, IDeclar
     public RoadPiece[] RoadPieces;
 
     private RoadNetworkGenerator roadNetworkGenerator;
-    
+
+    private EntityManager dstManager;
+    private List<Entity> roadSegments;
+    private Entity carEntityBase;
+
     public void Convert(Entity entity, EntityManager dstManager, GameObjectConversionSystem conversionSystem)
     {
         roadNetworkGenerator = new RoadNetworkGenerator(dstManager);
         using (BlobAssetStore blobAssetStore = new BlobAssetStore())
         {
-            Entity carPrefab = GameObjectConversionUtility.ConvertGameObjectHierarchy(CarPrefab,
+            this.carEntityBase =  GameObjectConversionUtility.ConvertGameObjectHierarchy(CarPrefab,
                 GameObjectConversionSettings.FromWorld(dstManager.World, blobAssetStore));
             
             roadNetworkGenerator.GenerateNetwork(RoadPieces, out var roadNodes, out var roadSegments);
 
-            SpawnCars(dstManager, carPrefab, roadSegments);
+            this.roadSegments = roadSegments;
+            this.dstManager = dstManager;
+            SpawnCars(dstManager, carEntityBase, roadSegments);
         }
     }
 
-    private void SpawnCars(EntityManager dstManager, Entity carPrefab, List<Entity> roadSegments)
+    private void SpawnCars(EntityManager dstManager, Entity carEntity, List<Entity> roadSegments)
     {
         for (int i = 0; i < CarToSpawn; i++)
         {
-            var carEntity = dstManager.Instantiate(carPrefab);
+            SpawnCar();
+        }
+    }
 
-            var vehicleComponent = dstManager.GetComponentData<VehiclePositionComponent>(carEntity);
-            var vehicleSegmentInfoComponent = dstManager.GetComponentData<VehicleSegmentInfoComponent>(carEntity);
-            var vehicleMoveIntentionComponent = dstManager.GetComponentData<VehicleSegmentChangeIntention>(carEntity);
-            var vehicleConfig = dstManager.GetComponentData<VehicleConfigComponent>(carEntity);
-
-            var segmentIndex = Random.Range(0, roadSegments.Count);
-            var segmentEntity = roadSegments[segmentIndex];
-            var segmentComponent = dstManager.GetComponentData<SegmentConfigComponent>(segmentEntity);
-
-            vehicleComponent.HeadSegPos = vehicleConfig.Length + 0.2f;
-            vehicleComponent.BackSegPos = vehicleComponent.HeadSegPos - vehicleConfig.Length;
-
-            vehicleSegmentInfoComponent.HeadSegment = segmentEntity;
-            vehicleSegmentInfoComponent.IsBackInPreviousSegment = false;
-            vehicleSegmentInfoComponent.PreviousSegment = Entity.Null;
-            vehicleSegmentInfoComponent.SegmentLength = segmentComponent.Length;
-            vehicleSegmentInfoComponent.NextNode = segmentComponent.EndNode;
-            
-            var nodeBuffer = dstManager.GetBuffer<ConnectedSegmentBufferElement>(segmentComponent.EndNode);
-            if (nodeBuffer.Length > 0)
-            {
-                var randomNextSegment = Random.Range(0, nodeBuffer.Length);
-                vehicleMoveIntentionComponent.NextSegment = nodeBuffer[randomNextSegment].segment;
-            }
-            
-            dstManager.SetComponentData(carEntity, vehicleComponent);
-            dstManager.SetComponentData(carEntity, vehicleSegmentInfoComponent);
-            dstManager.SetComponentData(carEntity, vehicleMoveIntentionComponent);
+    private void Update()
+    {
+        if (Input.GetMouseButtonUp(0))
+        {
+            SpawnCar();
         }
     }
 
     public void DeclareReferencedPrefabs(List<GameObject> referencedPrefabs)
     {
         referencedPrefabs.Add(CarPrefab);
+    }
+
+    private void SpawnCar()
+    {
+        var carEntity = dstManager.Instantiate(carEntityBase);
+        
+        var vehicleComponent = dstManager.GetComponentData<VehiclePositionComponent>(carEntity);
+        var vehicleSegmentInfoComponent = dstManager.GetComponentData<VehicleSegmentInfoComponent>(carEntity);
+        var vehicleMoveIntentionComponent = dstManager.GetComponentData<VehicleSegmentChangeIntention>(carEntity);
+        var vehicleConfig = dstManager.GetComponentData<VehicleConfigComponent>(carEntity);
+
+        var segmentEntity = GetRandomSegmentWithFreeSpace(vehicleConfig.Length/2, vehicleConfig.Length);
+        var segmentComponent = dstManager.GetComponentData<SegmentConfigComponent>(segmentEntity);
+        var splineComponent = dstManager.GetComponentData<SplineComponent>(segmentEntity);
+
+        vehicleComponent.HeadSegPos = vehicleConfig.Length;
+        vehicleComponent.BackSegPos = vehicleComponent.HeadSegPos - vehicleConfig.Length;
+
+        vehicleSegmentInfoComponent.HeadSegment = segmentEntity;
+        vehicleSegmentInfoComponent.IsBackInPreviousSegment = false;
+        vehicleSegmentInfoComponent.PreviousSegment = Entity.Null;
+        vehicleSegmentInfoComponent.SegmentLength = segmentComponent.Length;
+        vehicleSegmentInfoComponent.NextNode = segmentComponent.EndNode;
+            
+        var nodeBuffer = dstManager.GetBuffer<ConnectedSegmentBufferElement>(segmentComponent.EndNode);
+        if (nodeBuffer.Length > 0)
+        {
+            var randomNextSegment = Random.Range(0, nodeBuffer.Length);
+            vehicleMoveIntentionComponent.NextSegment = nodeBuffer[randomNextSegment].segment;
+        }
+        
+        var centerVehicleSegPos = vehicleComponent.HeadSegPos - vehicleConfig.Length / 2;
+        var currentPos = splineComponent.Point(centerVehicleSegPos / splineComponent.Length);
+        var nextPos = splineComponent.Point(centerVehicleSegPos + 0.1f / splineComponent.Length);
+        var directionVector = nextPos - currentPos;
+
+        dstManager.SetComponentData(carEntity, new Translation{Value = splineComponent.Point(centerVehicleSegPos / splineComponent.Length)});
+        dstManager.SetComponentData(carEntity, new Rotation {Value = quaternion.LookRotation(directionVector, math.up())});
+        dstManager.SetComponentData(carEntity, vehicleComponent);
+        dstManager.SetComponentData(carEntity, vehicleSegmentInfoComponent);
+        dstManager.SetComponentData(carEntity, vehicleMoveIntentionComponent);
+    }
+
+    private Entity GetRandomSegmentWithFreeSpace(float position, float size)
+    {
+        var vehiclesSegmentsHashMap = CalculateCarsInSegmentsSystem.VehiclesSegmentsHashMap;
+        var helper = new VehiclesInSegmentHashMapHelper();
+
+        for (int i = 0; i < 10; i++)
+        {
+            var segmentIndex = Random.Range(0, roadSegments.Count);
+            var segmentEntity = roadSegments[segmentIndex];
+            if (helper.IsSpaceAvailableAt(vehiclesSegmentsHashMap, segmentEntity, position, size))
+                return segmentEntity;
+        }
+        
+        return Entity.Null;;
     }
 }
